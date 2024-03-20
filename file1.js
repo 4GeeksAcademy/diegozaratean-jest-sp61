f252a00611ae5c15667ff403de2bcc9d5a59382f
diego <diegozaratean@gmail.com>
2024-03-20 20:29:40 +0000

    add import export exampels

---
+++ file1.js
@@ -0,0 +1,8 @@
+//importar funcion sum
+import { sum,substract } from './file2.js'
+import unica from './file2.js'
+
+//ejecutar y mostrar en cosonla el resutlado de la funcion sum
+console.log(sum(2,3))
+console.log(substract(7,1))
+console.log(unica(4,9))
---
+++ file2.js
@@ -0,0 +1,21 @@
+// crear la funcion sum
+let sum = (a,b)=>{
+    return a + b;
+};
+
+
+let substract = (a,b)=>{
+    return a - b;
+};
+
+let unica = (a,b)=>{
+    return a * b;
+};
+
+// exportar la funcion sum
+export {
+    sum,
+    substract
+}
+
+export default unica
